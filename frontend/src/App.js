9b5cba4aed271b52db7892df50032879ab9d3f22
Jacob Harris <jacobaharris05@gmail.com>
2025-07-31 21:52:56 -0400

    Revert "Phishing Demo implimented"
    This reverts commit 99ef17f351e1721cd42eabe752cd1ac5a2322da9.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I don't know frontend stuff too well but I will learn
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
