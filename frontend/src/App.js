2e1dd5d412b38b67385592c46f8abc8d8ef4c04a
Jacob Harris <jacobaharris05@gmail.com>
2025-07-31 21:52:50 -0400

    Revert "Base GUI + Spring boot setup"
    This reverts commit 1f7ff54fe84dd9e346862dbd67d6f41e75b9cc3d.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>There are a variety of different methods to crack a password, experiment
-          with different attack types and hash combinations to learn more about each cracking
-          technique!
-        </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I don't know frontend stuff too well but I will learn
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-
-      <div className="button-grid">
-        <BruteForceButton />
-        <DictionaryButton />
-        <BfDictionaryButton />
-        <RainbowTableButton />
-        <PhishingButton />
-      </div>
-
-
     </div>
   );
 }
 
-function BruteForceButton() {
-  return (
-    <p>
-      <button>Brute Force</button>
-    </p>
-  );
-}
-
-function DictionaryButton() {
-  return (
-    <p>
-      <button>Dictionary</button>
-    </p>
-  );
-}
-
-function BfDictionaryButton() {
-  return (
-    <p>
-      <button>Brute Force + Dictionary</button>
-    </p>
-  );
-}
-
-function RainbowTableButton() {
-  return (
-    <p>
-      <button>Rainbow Table</button>
-    </p>
-  );
-}
-
-function PhishingButton() {
-  return (
-    <p>
-      <button>Phishing Simulation</button>
-    </p>
-  );
-}
-
-
+export default App;
