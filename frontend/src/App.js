a36112657d5fa5bcef78b33ee8ed333c223acba7
Jacob Harris <jacobaharris05@gmail.com>
2025-07-31 21:52:33 -0400

    Revert "imporved SpringBoot/React acessability"
    This reverts commit 7586bac2c8f10fd2b5b28437267accfd4461073b.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
-  const [status, setStatus] = useState('Loading...');
-
-  useEffect(() => {
-    fetch('http://localhost:8080/status')
-      .then(res => res.text())
-      .then(data => setStatus(data))
-      .catch(err => {
-        console.error(err);
-        setStatus('Error fetching status');
-      });
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
@@ -21,10 +9,6 @@ export default function App() {
           with different attack types and hash combinations to learn more about each cracking
           technique!
         </h1>
-        <h3>
-          Backend status: 
-        </h3>
-         <p>{status}</p>
       </header>
 
       <div className="button-grid">
